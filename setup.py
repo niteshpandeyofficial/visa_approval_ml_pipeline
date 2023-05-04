2bdee245d8860815ff9cb73e4d90dba6e7f72ab2
Nitesh <niteshpandeyofficial@gmail.com>
2023-05-04 12:32:24 +0530

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup,find_packages
 from distutils.core import setup
+from typing import List
 
 PROJECT_NAME='Machine Learning End To End Project'
 VERSION="0.0.1"
 AUTHOR="Nitesh Pandey"
 DESCRIPTION="This is complete ened to end machine learning project"
+
+
+HYPHEN_E_DOT="-e ."
+def get_requirements_list()->List(str):
+    with open('requirements.txt','r') as req_file:
+        req_list=req_file.readlines()
+        req_list=[req_name.replace("\n","") for req_name in req_list]
+        if HYPHEN_E_DOT in req_list:
+            req_list.remove(HYPHEN_E_DOT)
+        return req_list
+
 
 setup(
     name=PROJECT_NAME,
