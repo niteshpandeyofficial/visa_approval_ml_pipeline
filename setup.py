e94b2e06fe00f6fbe4508308be6fdd14db19b123
Nitesh <niteshpandeyofficial@gmail.com>
2023-05-04 12:22:39 +0530

    setup file details added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+from distutils.core import setup
+
+PROJECT_NAME='Machine Learning End To End Project'
+VERSION="0.0.1"
+AUTHOR="Nitesh Pandey"
+DESCRIPTION="This is complete ened to end machine learning project"
+
+setup(
+    name=PROJECT_NAME,
+    author=AUTHOR,
+    version=VERSION,
+    description=DESCRIPTION,
+    author_email='niteshpandeyofficial@gmail.com'
+    )
